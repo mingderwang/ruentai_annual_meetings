03728dfa4aa279058f813f0e7efef2a50f48aac1
Ming-der Wang <mingderwang@gmail.com>
2021-09-18 23:06:26 +0800

    add ipns

--- ipfs.js
+++ ipfs.js
@@ -4,11 +4,11 @@ const { clearLine } = require("readline");
 
 const { globSource } = ipfsAPI;
 
-//const infura = { host: "ipfs.infura.io", port: "5001", protocol: "https" };
+const infura = { host: "ipfs.infura.io", port: "5001", protocol: "https" };
 // run your own ipfs daemon: https://docs.ipfs.io/how-to/command-line-quick-start/#install-ipfs
-const localhost = { host: "localhost", port: "5001", protocol: "http" };
+//const localhost = { host: "localhost", port: "5001", protocol: "http" };
 
-const ipfs = ipfsAPI(localhost);
+const ipfs = ipfsAPI(infura);
 
 const ipfsGateway = "https://ipfs.io/ipfs/";
 
